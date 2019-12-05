ec73fe732dde0e1fe11d6c09d07dbdf360f2c672
prnakka099 <prnakka@deloitte.com>
2019-12-05 14:54:02 -0500

    package nessus

--- dcp-componentization/images/openshift-test/mycustom.lang.sh
+++
@@ -1,5 +0,0 @@
-## US English ##
-export LANG=en_US.UTF-8
-export LANGUAGE=en_US.UTF-8
-export LC_COLLATE=C
-export LC_CTYPE=en_US.UTF-8
--- dcp-componentization/images/selenium/Selenium-install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo yum install -y java-1.8.0 
-wget http://selenium-release.storage.googleapis.com/3.10/selenium-server-standalone-3.10.0.jar
